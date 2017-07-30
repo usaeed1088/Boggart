dba97f341265c13c77d7764b3aa3e49ccb6ea1ca
Usman Saeed <usman-syed@live.com>
2017-07-30 16:00:15 +1000

    - Added TCP Client and Server Transport

--- Lib/Transport/TCP/Client/Client.h
+++ Lib/Transport/TCP/Client/Client.h
@@ -2,6 +2,8 @@
 
 #include "../../TransportBase.h"
 
+#include <Lib/Object/Objects/ISocket.h>
+
 #include <string>
 #include <cstdint>
 
@@ -17,8 +19,20 @@ namespace Boggart
 				std::string m_ServerIP;
 				std::int32_t m_ServerPort;
 
-			public:
+				PAL::Object::ISocketPtr m_Socket;
 
+			public:
+				Client(std::string myId, std::string serverIP, std::int32_t serverPort);
+				~Client() override;
+
+			private:
+				bool OnOpen() override;
+
+				bool OnClose() override;
+
+				bool OnSend(const std::vector<unsigned char>& data) override;
+
+				std::vector<unsigned char> OnReceive() override;
 			};
 		}
 	}
